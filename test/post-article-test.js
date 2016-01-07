c6272d896a46c2906915c6031586dd2bef99b9a2
Frode Bjerke <frode.bjerke@gmail.com>
2016-01-07 22:22:28 +0100

    Posting an article returns article with a mock id and a completedAt date

--- test/post-article-test.js
+++ test/post-article-test.js
@@ -1,12 +1,27 @@
 import request from "supertest";
+import moment from 'moment';
 import app from "../lib/app";
+
 
 describe('post article', () => {
 
-    it('respond with hello world', (done) => {
+    const article = {
+        title: "On lava lamps",
+        exerpt: "What are lava lamps really good for?",
+        imageUrl: "",
+        url: "life.frode.space",
+        addedAt: moment('1994-01-01')
+    };
+
+    it('article with completedAt and id returned', (done) => {
         request(app)
             .post('/v1/articles')
-            .expect('hello world', done);
+            .set('Content-Type', 'application/json')
+            .send(article)
+            .expect('Content-Type', /application\/json/)
+            .expect(/"id":/)
+            .expect(/"completedAt":/)
+            .expect(200, done);
     })
 
 })
